18a482d7aa84d2c150b9d9fa6925f2790694d732
saeidnahali <63702251+saeidnahali@users.noreply.github.com>
2020-12-02 20:27:05 -0800

    Website

--- Pandas.py
+++ Pandas.py
@@ -7,9 +7,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 import matplotlib.pyplot as pp
 
-# data=pd.read_html("https://www.kaggle.com/chrisbow/2018-calorie-exercise-and-weight-changes")
-
-data = pd.read_csv("diet_data.csv")  # imports the data from csv
+data= "https://storage.googleapis.com/kagglesdsdata/datasets/100171/236755/diet_data.csv?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20201203%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20201203T030243Z&X-Goog-Expires=259199&X-Goog-SignedHeaders=host&X-Goog-Signature=01aac892dce1549e4ddd02c5cbffb6a11cd2d9aaf46c745afdd3c0656e24d35fb648c08f1fe186ef0f834394ffe5c2cb2527b92d3aef2e73f943b2c5800b4e666424603867b41951f2575788844ee9e9ae4341e16bf57b6fa5ae212b950c00d8ca455602a44c4e95ad25556dd83a99f63a20bf1d4f529510d0405328c865e645f15ebad67b4c675723be6082bc050c54a41a515edfb5128e6b81a0679850ab90714fe0f03cc4c89e4e83feeb19cbefdec05f54858cf9c4af55d1452882886bd6a01e82b842e1b625929b149f594ed76b192e725b21b471ebfe8787a91d6101f23fbac813d8de980d98dade041aaf388c35ecc4b92835df4ec3372366182a01f5"
+data = pd.read_csv(data)
+# data = pd.read_csv("diet_data.csv")  # imports the data from csv
 
 print(data.columns)    # prints columns from csv
 
